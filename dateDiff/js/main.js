56c8fb98d94ff78a030f01ba3c77f2f24565a702
Anton Ryabtsev <anton_ryabcev@mail.ru>
2021-12-25 23:55:02 +0300

    realised hidden function

--- dateDiff/js/main.js
+++ dateDiff/js/main.js
@@ -18,3 +18,23 @@ form.onsubmit = (event) => {
     printResult(dateDiff);
   }
 };
+
+// home task 1
+
+const datecalc = document.querySelector('#datecalc');
+const timer = document.querySelector('#timer');
+
+const el = document.querySelector('body').addEventListener('click', event => {
+  if (event.target.classList == 'calc--btn') {
+    getHidden();
+  } else if (event.target.classList == 'timer--btn') {
+    getHidden();
+  } else {
+    return
+  }
+});
+
+function getHidden() {
+  datecalc.classList.toggle('active')
+  timer.classList.toggle('active')
+}
